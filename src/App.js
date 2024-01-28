754916469c6349a1908657148d54baeaa3acb4b3
NHnew nabi <nbihuseynov1@gmail.com>
2024-01-28 21:35:25 +0000

    changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import { useState } from "react";
 
 function App() {
-  const [text, setText] = useState("Salam, mən ");
+  const [text, setText] = useState();
   return (
     <div className="app">
-      <h1>{text}</h1>
-      <button onClick={() => setText((t => t + "adınız-am."))}>0</button>
-      <button onClick={() => setText((t => t + "əlaçıyam."))}>1</button>
-      <button onClick={() => setText((t => t + "yaxşı oxuyanam."))}>2</button>
-      <button onClick={() => setText((t => t + "orta oxuyanam."))}>3</button>
+      <h1>Salam,mən {text}</h1>
+      <button onClick={() => setText("Nəbiyəm.")}>0</button>
+      <button onClick={() => setText("əlaçıyam.")}>1</button>
+      <button onClick={() => setText("yaxşı oxuyanam.")}>2</button>
+      <button onClick={() => setText("orta oxuyanam.")}>3</button>
     </div >
   );
 }
